2380fa41f9de16fcc119538557ff891612bbf1a5
Nicolas Noé <nicolas@niconoe.eu>
2018-09-24 15:37:23 +0200

    Basic admin site running (+ forgotten migration file).

--- djangoproject/vespawatch/admin.py
+++ djangoproject/vespawatch/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Species, Observation, ObservationPicture, ManagementAction
+
+@admin.register(Species)
+class SpeciesAdmin(admin.ModelAdmin):
+    pass
+
+
+class PictureInline(admin.TabularInline):
+    model = ObservationPicture
+
+
+@admin.register(Observation)
+class ObservationAdmin(admin.ModelAdmin):
+    inlines = [
+        PictureInline,
+    ]
+
+@admin.register(ManagementAction)
+class ManagementActionAdmin(admin.ModelAdmin):
+    pass
--- djangoproject/vespawatch/models.py
+++ djangoproject/vespawatch/models.py
@@ -4,6 +4,12 @@ from django.db import models
 class Species(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return  self.name
+
+    class Meta:
+        verbose_name_plural = "species"
+
 
 class Observation(models.Model):
     NEST = 'NE'
