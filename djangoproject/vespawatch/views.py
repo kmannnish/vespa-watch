a12d0eb455e2881caf65ec579b74342aa89be8ce
bartaelterman <bart.aelterman@gmail.com>
2018-10-09 10:50:14 +0200

    Add comments to views file

--- djangoproject/vespawatch/views.py
+++ djangoproject/vespawatch/views.py
@@ -2,11 +2,19 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from .models import Observation
 
+
 def index(request):
     return render(request, 'vespawatch/index.html')
 
 
+# ==============
+# API methods
+# ==============
+
 def observations_json(request):
+    """
+    Return all observations as json data
+    """
     observations = Observation.objects.all()
     return JsonResponse({
         'observations': [x.as_dict() for x in observations]
