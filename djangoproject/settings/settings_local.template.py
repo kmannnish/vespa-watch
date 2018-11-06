14cabb5bc5ba6a9c327acafb76477aa867f67f35
stijnvanhoey <stijnvanhoey@gmail.com>
2018-11-06 14:42:53 +0100

    Update S3 config to settings info

--- djangoproject/settings/settings_local.template.py
+++ djangoproject/settings/settings_local.template.py
@@ -14,7 +14,21 @@ SECRET_KEY = '<SOMETHING_SECRET_TO_REDEFINE_HERE>'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+# INaturalist user information
 INAT_USER_USERNAME = 'vespawatch'
 INAT_USER_PASSWORD = ''
 INAT_APP_ID = 'd1d0f541791be42e234ce82a5bb8332ab816ff7ab35c6e27b12c0455939a5ea8'
 INAT_APP_SECRET = ''
+
+# media file S3 static storage
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_ACCESS_KEY_ID = None # None to use AWS internal role/permissions
+AWS_ACCESS_KEY_ID = None
+AWS_STORAGE_BUCKET_NAME = 'lw-vespawatch'
+AWS_DEFAULT_ACL = None
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',  # 1 day
+}
+AWS_S3_FILE_OVERWRITE = True
+AWS_S3_REGION_NAME = ' eu-west-1'
+AWS_LOCATION = 'media'
