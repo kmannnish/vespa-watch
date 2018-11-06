9943cf8c110d8d413a6234f3e9dc3b04efacc929
bartaelterman <bart.aelterman@gmail.com>
2018-11-06 22:08:38 +0100

    initialize translations in javascript

--- djangoproject/urls.py
+++ djangoproject/urls.py
@@ -19,11 +19,14 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.contrib.auth import views as auth_views
+from django.views.i18n import JavaScriptCatalog
 
 urlpatterns = [
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
+
+    path('jsi18n/', JavaScriptCatalog.as_view(), name='javascript-catalog'),
 
     path('', include('vespawatch.urls')),
 
