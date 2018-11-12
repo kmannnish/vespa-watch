ad5a1776b11b3bc39fe7d2369f193ea76d939287
Nicolas Noé <nicolas@niconoe.eu>
2018-11-12 12:00:03 +0100

    Basic GeoDjango setup.

--- djangoproject/settings/settings_local.template.py
+++ djangoproject/settings/settings_local.template.py
@@ -2,7 +2,7 @@ from .base import *
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
         'NAME': 'vespa-watch',
         'HOST': 'localhost',
     }
--- vespawatch/models.py
+++ vespawatch/models.py
@@ -6,7 +6,7 @@ import dateparser
 from django.conf import settings
 from django.contrib.auth.models import User
 from django.contrib.postgres.fields import ArrayField
-from django.db import models
+from django.contrib.gis.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.template import defaultfilters
@@ -349,6 +349,7 @@ class ManagementAction(models.Model):
 
 class FirefightersZone(models.Model):
     name = models.CharField(max_length=100)
+    polygon = models.PolygonField(null=True)
 
     def __str__(self):
         return self.name
