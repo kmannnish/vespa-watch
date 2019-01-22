edbea8542ba4957a33d576290cddb1c6050fb44a
Peter Desmet <peter.desmet.work@gmail.com>
2019-01-22 11:14:35 +0100

    Group and reorder custom settings

--- djangoproject/settings/base.py
+++ djangoproject/settings/base.py
@@ -124,13 +124,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'Europe/Brussels'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 LANGUAGES = [
@@ -143,38 +139,11 @@ LANGUAGES_AVAILABLE_IN_SELECTOR = [
     ('en', _('English')),
 ]
 
-LOCALE_PATHS = (os.path.join(BASE_DIR, 'locale'), )
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.1/howto/static-files/
-
-STATIC_URL = '/static/'
-MEDIA_URL = '/img/'
-
-VESPAWATCH_PROJECT_ID = 22865  # Vespa-Watch project ID @ iNaturalist
-VESPAWATCH_USER_ID = 1263313  # vespawatch user ID @ iNaturalist
-
-WEBSITE_NAME = "Vespa-Watch"
-
-SETTINGS_EXPORT = [
-    'DEBUG',
-    'JS_DEBUG',
-    'WEBSITE_NAME',
-    'LANGUAGES',
-    'LANGUAGES_AVAILABLE_IN_SELECTOR',
-    'VESPAWATCH_ID_OBS_FIELD_ID',
-    'VESPAWATCH_EVIDENCE_OBS_FIELD_ID'
-]
-
 PAGE_FRAGMENTS_FALLBACK_LANGUAGE = 'nl'
 
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
+LOCALE_PATHS = (os.path.join(BASE_DIR, 'locale'), )
 
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/'
-
-VESPAWATCH_ID_OBS_FIELD_ID = 9613 # # The identifier of the "vespawatch_id" observation field @iNaturalist
-VESPAWATCH_EVIDENCE_OBS_FIELD_ID = 9770  # The identifier of the "vespawatch_evidence" observation field @iNaturalist
+# Design for JS
 
 MAP_CIRCLE_FILL_OPACITY = 0.5
 MAP_CIRCLE_STROKE_OPACITY = 0.8
@@ -198,4 +167,33 @@ MAP_TILELAYER_OPTIONS = {
     'maxZoom': 20
 }
 
+# Local paths/URLs
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+MEDIA_URL = '/img/'
+STATIC_URL = '/static/'
+
+# Other settings
+
+WEBSITE_NAME = "Vespa-Watch"
+
+VESPAWATCH_EVIDENCE_OBS_FIELD_ID = 9770  # The identifier of the "vespawatch_evidence" observation field @iNaturalist
+VESPAWATCH_ID_OBS_FIELD_ID = 9613 # # The identifier of the "vespawatch_id" observation field @iNaturalist
+VESPAWATCH_PROJECT_ID = 22865  # vespawatch project ID @ iNaturalist
+VESPAWATCH_USER_ID = 1263313  # vespawatch user ID @ iNaturalist
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 JS_DEBUG = False
+
+# Settings that should be available in the templates
+
+SETTINGS_EXPORT = [
+    'DEBUG',
+    'JS_DEBUG',
+    'LANGUAGES',
+    'LANGUAGES_AVAILABLE_IN_SELECTOR',
+    'VESPAWATCH_EVIDENCE_OBS_FIELD_ID',
+    'VESPAWATCH_ID_OBS_FIELD_ID',
+    'WEBSITE_NAME'
+]
