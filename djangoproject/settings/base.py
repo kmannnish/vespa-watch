66c3ff8f0fdd78748e5cc4a819e0d2da6b50f65a
Peter Desmet <peter.desmet@inbo.be>
2019-03-19 10:59:15 +0100

    Update default colour of nests on map to green

--- djangoproject/settings/base.py
+++ djangoproject/settings/base.py
@@ -194,7 +194,7 @@ MAP_CIRCLE_INDIVIDUAL_COLOR = '#FD9126'
 MAP_CIRCLE_NEST_COLOR = {  # This depend of the management action
     'finished': '#9CCB19',
     'unfinished': '#EE4000',
-    'DEFAULT': '#3678ff'
+    'DEFAULT': '#73984A'
 }
 MAP_CIRCLE_UNKNOWN_COLOR = '#000' # if the subject is not 'Individual' or 'Nest'
 MAP_INITIAL_POSITION = [50.85, 4.35]
