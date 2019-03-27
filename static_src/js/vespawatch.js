07df12b006ad01f8d83422e6ecbf662320cb83bc
Peter Desmet <peter.desmet.work@gmail.com>
2019-03-27 14:52:35 +0100

    Include imageLoader.js into vespawatch.js + use document.ready

--- static_src/js/vespawatch.js
+++ static_src/js/vespawatch.js
@@ -5,11 +5,27 @@
 
 // 1. Global stuff
 
-// Enable the language selector (Navbar)
+// Enable the language selector (navbar)
 $(document).ready(function () {
     $('#lang').on('change', function () {
         document.forms['lang-form'].submit();
     });
+});
+
+// Load obs_card images
+$(document).ready(function () {
+    $('.theme-card-deck div.card').each(function (index, value) {
+        var card = $(this);
+        var id = card.attr('id').split('-')[2];
+        var obj = card.find('.card-text span.badge').text().toLowerCase();
+        $.getJSON('/api/' + obj + 's/' + id)
+            .done(function (r) {
+                if (r.thumbnails && r.thumbnails.length > 0 && r.thumbnails[0]) {
+                    card.find('img.card-img-top').attr('src', r.thumbnails[0]);
+                }
+            });
+        }
+    );
 });
 
 // Disable console.log() et al. if settings.JS_DEBUG != True
@@ -23,7 +39,6 @@ if (!VWConfig.debug) {
 }
 
 // 2. Vue.JS components
-
 
 // The map of the visualization.
 // This contains an observations prop. When this property is updated, (when data is retrieved
